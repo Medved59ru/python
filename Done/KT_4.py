3f5cfaf55cfa1b510f9db6de4394e383ac7d13ba
Aleksandr Figilyantov <a.figilyantov@gmail.com>
2021-11-01 11:00:37 +0500

    KT 4 full version

--- Done/KT_4.py
+++ Done/KT_4.py
@@ -1,15 +1,22 @@
-from tkinter import WORD, Button, Tk, END, Text
+import re
 import bs4
 import requests
-import re
+import tkinter as tk
 
 URL = r"http://lib.ru/PROZA/"
 
-'''response = requests.get(URL)
-soup = bs4.BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('small', class_='b')
-print(quotes)'''
-
+def bsParse():
+    response = requests.get(URL)
+    htmlCode = response.text
+    soup = bs4.BeautifulSoup(htmlCode, features="lxml")
+    tags = soup.find_all("li")[6:]
+    rows =""
+    for li in tags:
+        author = li.find_all("b")[1].text
+        number = li.find_all("small")[0].text
+        number = re.sub("[(|)|\[|\]|dir|www]", "", number)
+        rows += f"{number}\t{author}\n"
+    return rows.strip()
 
 def reParse():
     response = requests.get(URL)
@@ -21,16 +28,17 @@ def reParse():
 
 def buttonClick(ctrl, method):
     """Универсальный обработчик нажатия кнопки"""
-    ctrl.delete("1.0", END)
-    ctrl.insert(END,  reParse())
+    ctrl.delete("1.0", tk.END)
+    ctrl.insert(tk.END, bsParse() if method == "bs" else reParse())
 
-form = Tk()
-form.title("КТ 4, вариант 1")
-editText = Text(height=30, width=70, wrap=WORD)
+
+form = tk.Tk()
+form.title("КТ 4, вариант 7")
+editText = tk.Text(height=30, width=70, wrap=tk.WORD)
 editText.pack()
-# bsButton = Button(text="Спарсить библиотекой", command=lambda: buttonClick(editText, method="bs"))
-# bsButton.pack()
-reButton = Button(text="Спарсить регулярками", command=lambda: buttonClick(editText, method="re"))
+bsButton = tk.Button(text="Спарсить библиотекой", command=lambda: buttonClick(editText, method="bs"))
+bsButton.pack()
+reButton = tk.Button(text="Спарсить регулярками", command=lambda: buttonClick(editText, method="re"))
 reButton.pack()
 
 form.mainloop()
